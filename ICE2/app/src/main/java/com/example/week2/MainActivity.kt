e8f1423af015766fa9e7a179f0db789c59bb68ec
junior123w <113151392+junior123w@users.noreply.github.com>
2024-01-19 19:50:58 -0500

    doing till  step 3

--- ICE2/app/src/main/java/com/example/week2/MainActivity.kt
+++ ICE2/app/src/main/java/com/example/week2/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.week2
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val greetingTextView = findViewById<TextView>(R.id.greetingTextView)
+        greetingTextView.text="Hello, World"
+        val allCapsButton = findViewById<Button>(R.id.change)
+
+        allCapsButton.setOnClickListener{greetingTextView.text=if(greetingTextView.text == "Hello, World") "GoodBye , World"
+        else "Hello, World"
+        }
     }
 }
