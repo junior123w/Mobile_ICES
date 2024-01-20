28956e07c0d86ed460eb09a247283dda87234d50
junior123w <113151392+junior123w@users.noreply.github.com>
2024-01-19 20:22:35 -0500

    adding layouts and logs

--- ICE2/app/src/main/java/com/example/week2/MainActivity.kt
+++ ICE2/app/src/main/java/com/example/week2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.week2
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.annotation.RequiresApi
@@ -12,12 +13,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val greetingTextView = findViewById<TextView>(R.id.greetingTextView)
-        greetingTextView.text="Hello, World"
+        greetingTextView.text = "Hello, World"
+        Log.i("info","hello world soft coded in the textview")
         val allCapsButton = findViewById<Button>(R.id.change)
 
-        allCapsButton.setOnClickListener{greetingTextView.text=if(greetingTextView.text == "Hello, World") "GoodBye , World"
-        else "Hello, World"
+        allCapsButton.setOnClickListener {
+            greetingTextView.text = if (greetingTextView.text == "Hello, World") "GoodBye , World"
+            else "Hello, World"
+            Log.i("","making sure that button works")
         }
     }
 }
