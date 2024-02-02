3481fd8429b8da620a979b228fe75f145e7bdb37
junior123w <113151392+junior123w@users.noreply.github.com>
2024-02-02 18:40:28 -0500

    added initializeOnCLickListener function

--- ICE4/app/src/main/java/com/example/week4/MainActivity.kt
+++ ICE4/app/src/main/java/com/example/week4/MainActivity.kt
@@ -13,37 +13,48 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initializeOnCLickListener()
+    }
 
-        binding.clearButton.setOnClickListener { view -> processOperatorButtons(view) }
-        binding.percentButton.setOnClickListener { view -> processOperatorButtons(view) }
-        binding.backSpaceButton.setOnClickListener { view -> processOperatorButtons(view) }
+    private fun initializeOnCLickListener() {
+        //operator buttons
         binding.divideButton.setOnClickListener { view -> processOperatorButtons(view) }
         binding.addButton.setOnClickListener { view -> processOperatorButtons(view) }
         binding.subtractButton.setOnClickListener { view -> processOperatorButtons(view) }
-        binding.plusMinusButton.setOnClickListener { view -> processOperatorButtons(view) }
         binding.equalButton.setOnClickListener { view -> processOperatorButtons(view) }
         binding.multiplyButton.setOnClickListener { view -> processOperatorButtons(view) }
-        binding.nineButton.setOnClickListener { view-> numberButtons(view) }
-        binding.eightButton.setOnClickListener { view-> numberButtons(view) }
-        binding.sevenButton.setOnClickListener { view-> numberButtons(view) }
-        binding.sixButton.setOnClickListener { view-> numberButtons(view) }
-        binding.fiveButton.setOnClickListener { view-> numberButtons(view) }
-        binding.fourButton.setOnClickListener { view-> numberButtons(view) }
-        binding.thirdButton.setOnClickListener { view-> numberButtons(view) }
-        binding.twoButton.setOnClickListener { view-> numberButtons(view) }
-        binding.oneButton.setOnClickListener { view-> numberButtons(view) }
-        binding.zeroButton.setOnClickListener { view-> numberButtons(view) }
-        binding.decimalButton.setOnClickListener { view-> numberButtons(view) }
+
+        //extra buttons
+        binding.plusMinusButton.setOnClickListener { view -> processExtraButtons(view) }
+        binding.clearButton.setOnClickListener { view -> processExtraButtons(view) }
+        binding.percentButton.setOnClickListener { view -> processExtraButtons(view) }
+        binding.backSpaceButton.setOnClickListener { view -> processExtraButtons(view) }
+
+        //number buttons
+        binding.nineButton.setOnClickListener { view -> numberButtons(view) }
+        binding.eightButton.setOnClickListener { view -> numberButtons(view) }
+        binding.sevenButton.setOnClickListener { view -> numberButtons(view) }
+        binding.sixButton.setOnClickListener { view -> numberButtons(view) }
+        binding.fiveButton.setOnClickListener { view -> numberButtons(view) }
+        binding.fourButton.setOnClickListener { view -> numberButtons(view) }
+        binding.thirdButton.setOnClickListener { view -> numberButtons(view) }
+        binding.twoButton.setOnClickListener { view -> numberButtons(view) }
+        binding.oneButton.setOnClickListener { view -> numberButtons(view) }
+        binding.zeroButton.setOnClickListener { view -> numberButtons(view) }
+        binding.decimalButton.setOnClickListener { view -> numberButtons(view) }
     }
 
+    //shared event handlers
     private fun processOperatorButtons(view: View) {
        binding.resultTextView.text=view.tag.toString()
-        Log.i("operators",view.tag.toString())
+    }
+
+    private fun processExtraButtons(view:View){
+
     }
 
     private fun numberButtons(view: View) {
         binding.resultTextView.text=view.tag.toString()
-        Log.i("numbers",view.tag.toString())
     }
 
 }
